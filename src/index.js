ac541f4fb65dbe00d93a9b296d68120c5dbe40f8
ImanPunia <imanpunia19@gmail.com>
2020-07-15 23:07:30 +0530

    working observable example

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@
 exports.__esModule = true;
 var rxjs_1 = require("rxjs");
 var observable = new rxjs_1.Observable(function (observer) {
-    observer.next("Hello Rxjs");
-    observer.next(1);
+    observer.next("Rxjs");
+    var x = 0;
+    setTimeout(function () {
+        x = x + 1;
+        observer.next(x);
+    }, 1000);
 });
-observable.subscribe(function (x) { return console.log(x); });
+observable.subscribe(function (x) { return console.log("x", x); });
+observable.subscribe(function (y) { return console.log("y", y); });
+observable.subscribe(function (z) { return console.log("z", z); });
+function someFunction(a) {
+    a.next(1);
+}
+;
+var boundSomeFunction = new rxjs_1.Observable(someFunction);
+boundSomeFunction.subscribe(function (value) {
+    console.log("val", value);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-import { Observable } from 'rxjs';
+import { Observable, SubscribableOrPromise, Subscriber } from 'rxjs';
 
 var observable = new Observable(observer => {
-    observer.next("Hello Rxjs");
-    observer.next(1);
+    observer.next("Rxjs");
+    let x =0;
+    setTimeout(() => {
+        x=x+1;
+        observer.next(x);
+      }, 1000);
 })
 
-observable.subscribe((x) => console.log(x));
+
+observable.subscribe((x) => console.log("x",x));
+observable.subscribe((y) => console.log("y",y));
+observable.subscribe((z) => console.log("z",z));
+
+function observerFunction(a:Subscriber<unknown>){
+ a.next(1);
+  };
+  
+  const returnedObservable = new Observable(observerFunction);
+  
+  returnedObservable.subscribe(value => {
+    console.log("val",value)
+  });
