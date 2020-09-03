83c20cff77ca4b6791988d04afcd7b59b874dcdf
ImanPunia <imanpunia19@gmail.com>
2020-09-04 00:07:33 +0530

    removed the comments

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 "use strict";
 exports.__esModule = true;
-// // function cannot return multiple values
-// function foo1() {
-//   console.log("called foo1");
-//   return 3;
-//   return 4;
-// }
-// console.log(foo1());
-// //function acting like observable 
-// function foo() {
-//   console.log("called foo");
-//   returnMultiple(3);
-//   returnMultiple(4);
-// }
-// //like multiple execution of next function of Subscriber class from the registered callback
-// function returnMultiple(x:any) {
-//   console.log(x);
-// }
-// foo();
-// //simple observable example
-// const abc = new Observable((x:Subscriber<unknown>)=> {
-//     x.next(1);
-//     x.next(2);
-// })
-// abc.subscribe(x => console.log("mapped with next function of subsriber", x))
+var rxjs_1 = require("rxjs");
+// function cannot return multiple values
+function foo1() {
+    console.log("called foo1");
+    return 3;
+    return 4;
+}
+console.log(foo1());
+//function acting like observable 
+function foo() {
+    console.log("called foo");
+    returnMultiple(3);
+    returnMultiple(4);
+}
+//like multiple execution of next function of Subscriber class from the registered callback
+function returnMultiple(x) {
+    console.log(x);
+}
+foo();
+//simple observable example
+var abc = new rxjs_1.Observable(function (x) {
+    x.next(1);
+    x.next(2);
+});
+abc.subscribe(function (x) { return console.log("mapped with next function of subsriber", x); });
 //mapping of parameter passed to subscriber with registered callback in observable
 function def(a) {
     debugger;
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
 import { Observable, Subscriber } from 'rxjs';
 
-// // function cannot return multiple values
-// function foo1() {
-//   console.log("called foo1");
-//   return 3;
-//   return 4;
-// }
+// function cannot return multiple values
+function foo1() {
+  console.log("called foo1");
+  return 3;
+  return 4;
+}
 
-// console.log(foo1());
+console.log(foo1());
 
-// //function acting like observable 
-// function foo() {
-//   console.log("called foo");
-//   returnMultiple(3);
-//   returnMultiple(4);
-// }
+//function acting like observable 
+function foo() {
+  console.log("called foo");
+  returnMultiple(3);
+  returnMultiple(4);
+}
 
-// //like multiple execution of next function of Subscriber class from the registered callback
-// function returnMultiple(x:any) {
-//   console.log(x);
-// }
+//like multiple execution of next function of Subscriber class from the registered callback
+function returnMultiple(x:any) {
+  console.log(x);
+}
 
-// foo();
+foo();
 
-// //simple observable example
-// const abc = new Observable((x:Subscriber<unknown>)=> {
-//     x.next(1);
-//     x.next(2);
-// })
+//simple observable example
+const abc = new Observable((x:Subscriber<unknown>)=> {
+    x.next(1);
+    x.next(2);
+})
 
-// abc.subscribe(x => console.log("mapped with next function of subsriber", x))
+abc.subscribe(x => console.log("mapped with next function of subsriber", x))
 
 
 //mapping of parameter passed to subscriber with registered callback in observable
